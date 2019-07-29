2583e9af9d0725fac5448858dc9c9abe0577bdd1
Den Lebedinsky <Den.Lebedinsky@gmail.com>
2019-07-29 15:24:57 +0300

    fixed loss of focus on the input field

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DebounceInput } from "react-debounce-input";
 
 export default ({ items, edit, del }) => {
 
@@ -16,53 +17,60 @@ export default ({ items, edit, del }) => {
   }
 
   return (
-    <div className='container'>
-      <div className='row'>
-        <form className="col-md-12" data-testid='list_of_items'>
+    <div className="container">
+      <div className="row">
+        <form className="col-md-12" data-testid="list_of_items">
+          {items &&
+            items.map(item => (
+              <div
+                className=" d-flex align-items-end justify-content-center"
+                key={item.id}
+                data-testid={item.id}
+              >
+                <div className="form-group mr-1">
+                  <DebounceInput
+                    type="string"
+                    className="form-control"
+                    value={item.name}
+                    onChange={e => editItem(item.id, "name", e.target.value)}
+                    debounceTimeout={3000}
+                  />
+                </div>
+                <div className="form-group mr-1">
+                  <DebounceInput
+                    type="number"
+                    className="form-control"
+                    value={item.expenses}
+                    onChange={e =>
+                      editItem(item.id, "expenses", e.target.value)
+                    }
+                    debounceTimeout={3000}
+                  />
+                </div>
+                <div className="form-group mr-1">
+                  <DebounceInput
+                    type="number"
+                    className="form-control"
+                    value={item.incomes}
+                    onChange={e =>
+                      editItem(item.id, "incomes", e.target.value)
+                    }
+                    debounceTimeout={3000}
+                  />
+                </div>
 
-          {items && items.map(item => (
-            <div className=" d-flex align-items-end justify-content-center" key={item.id} data-testid={item.id}>
-              <div className="form-group mr-1">
-
-                <input
-                  type="string"
-                  className="form-control"
-                  value={item.name}
-                  onChange={(e) => editItem(item.id, 'name', e.target.value)}
-                  disabled
-                />
+                <div className="form-group">
+                  <button
+                    className="btn btn-outline-danger"
+                    onClick={() => deleteItem(item.id)}
+                  >
+                    X
+                  </button>
+                </div>
               </div>
-              <div className="form-group mr-1">
-
-                <input
-                  type="number"
-                  className="form-control"
-                  value={item.expenses}
-                  onChange={(e) => editItem(item.id, 'expenses', e.target.value)}
-                  disabled
-                />
-              </div>
-              <div className="form-group mr-1">
-
-                <input
-                  type="number"
-                  className="form-control"
-                  value={item.incomes}
-                  onChange={(e) => editItem(item.id, 'incomes', e.target.value)}
-                  disabled
-                />
-              </div>
-
-              <div className="form-group">
-                <button
-                  className="btn btn-outline-danger"
-                  onClick={() => deleteItem(item.id)}
-                >X</button>
-              </div>
-            </div>)
-          )}
+            ))}
         </form>
       </div>
     </div>
-  )
+  );
 }
